09c63b5925b69c201b8bcca22388a9a2f9e8f78d
Osama Hamad <osamahama2005@gmail.com>
2021-10-15 23:09:11 -0400

    add machin formula function

---
+++ machin_formula.py
@@ -0,0 +1,8 @@
+import math
+
+def machin():
+    #return 4*math.atan(math.degrees(1/5)) - math.atan(math.degrees(1/239))
+    pi = 4*math.atan(1/5) - math.atan(1/239)
+    return pi*4
+
+print(machin())
