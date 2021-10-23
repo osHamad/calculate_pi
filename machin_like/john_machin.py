3281fcc383806e61ca0f6605f0fc3aa3e2fcbc39
Osama Hamad <osamahama2005@gmail.com>
2021-10-23 12:37:47 -0400

    fix file name

--- machin_like/john_machin.py
+++ machin_like/john_machin.py
@@ -2,7 +2,7 @@ import math
 
 def machin():
     #return 4*math.atan(math.degrees(1/5)) - math.atan(math.degrees(1/239))
-    pi = 4*math.atan(1/5) - math.atan(1/239)
+    pi = 4*math.atan(1/49) - math.atan(1/239)
     return pi*4
 
 print(machin())
